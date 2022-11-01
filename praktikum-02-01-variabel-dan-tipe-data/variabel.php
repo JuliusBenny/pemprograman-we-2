6b9a33487cd7f8ba06e57afad02544b742f93082
JuliusBenny <julius20@mhs.akba.ac.id>
2022-11-01 16:13:45 +0800

    Commit Tambah

--- praktikum-02-01-variabel-dan-tipe-data/variabel.php
+++ praktikum-02-01-variabel-dan-tipe-data/variabel.php
@@ -1,17 +1,19 @@
 <?php
-/**
-* Ini adalah nilai awal variabel
-*/
-$nama = "benny";
-$usia = 20;
-echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya berusia
-".$usia." tahun<p>";
-/**
-* Ini adalah variabel dengan nilai yang diubah
-*/
-$nama = ISI NAMA ANDA;
-$usia = ISI USIA ANDA;
 
-echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya berusia
-".$usia." tahun<p>";
-?>
+/**
+ * Ini adalah nilai awal variabel
+ */
+$nama = "Pasnur";
+$usia = 42;
+echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya berusia ".$usia." tahun<p>";
+
+/**
+ * Ini adalah variabel dengan nilai yang diubah
+ */
+
+$nama = "Azizah";
+$usia = 20;
+
+echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya berusia ".$usia." tahun<p>";
+
+ ?>
